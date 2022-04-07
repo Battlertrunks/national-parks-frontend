b0bed231beb5b78d6aed8cabb6aa2dba2d24274f
Gavin <Gavinszczesniak@gmail.com>
2022-04-07 15:28:13 -0400

    Added a form to get user data to look for what they want and created a couple of models to represent the data the user is sending.

--- src/services/NSPServices.ts
+++ src/services/NSPServices.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import ActivitiesResponse from "../models/ActivitiesResponse";
 import TrendingCardModelResponse from "../models/TrendingCardModelResponse";
 
 const key: string = process.env.REACT_APP_NPS_KEY || "";
@@ -6,6 +7,14 @@ const key: string = process.env.REACT_APP_NPS_KEY || "";
 export const getThingsToDo = async (): Promise<TrendingCardModelResponse> => {
   return (
     await axios.get("https://developer.nps.gov/api/v1/parks?", {
+      params: { api_key: key },
+    })
+  ).data;
+};
+
+export const getActivities = async (): Promise<ActivitiesResponse> => {
+  return (
+    await axios.get("https://developer.nps.gov/api/v1/activities", {
       params: { api_key: key },
     })
   ).data;
