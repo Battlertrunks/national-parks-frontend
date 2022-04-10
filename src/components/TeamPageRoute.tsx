fed4ed2cb48ecd867c7da1c306c015b32bbb55cd
Gavin <Gavinszczesniak@gmail.com>
2022-04-10 11:55:19 -0400

    Added a equal opportunity section and special thanks section to the team page.

--- src/components/TeamPageRoute.tsx
+++ src/components/TeamPageRoute.tsx
@@ -40,6 +40,43 @@ const TeamPageRoute = () => {
           </p>
         </li>
       </ul>
+      <h2>We treat our members with equal opportunity</h2>
+      <p>
+        At National Treasure we value our employees, we strive to become an
+        example of a diverse workforce. We support employee growth and
+        opportunity at National Treasure.
+      </p>
+      <h2>Special Thanks</h2>
+      <p>The team want to give speacial thanks to...</p>
+      <ul>
+        <li>
+          <h3>Mitch Cuckovich and Andrea Hoffman</h3>
+          <p>
+            Thank you for coming on and helping us after class time during our
+            time of need. We really appreciate you sharing your knowledge and
+            wisdom around programming and web developement. We hope to talk to
+            you more after the bootcamp is over to share our experiences after
+            and here what you have done as well.
+          </p>
+        </li>
+        <li>
+          <h3>Career Service Team</h3>
+          <p>
+            Thank you for taking your time for us to schedule meetings on
+            one-on-ones with and doing softskills to boost our value as a
+            developer. We look forward to you asking us for updates on our jobs!
+          </p>
+        </li>
+        <li>
+          <h3>Carmen Scott</h3>
+          <p>
+            Thank you Carmen for coming on during our class time bi-weekly to
+            ask us our concerns and thoughts. You made us feel like our voices
+            mattered. Big thanks for your support and I am sure the next class
+            will appreciate it!
+          </p>
+        </li>
+      </ul>
     </section>
   );
 };
