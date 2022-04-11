697a2560d5c2cfad0a673d5ceb5a4b5db1f9fa4a
Gavin <Gavinszczesniak@gmail.com>
2022-04-11 14:10:02 -0400

    Dropdown FAQ on the account page when the user clicks dropdown button.

--- src/components/AccountRoute.tsx
+++ src/components/AccountRoute.tsx
@@ -10,6 +10,12 @@ const Account = () => {
   const { user } = useContext(AuthContext);
   const { attendedParks } = useContext(AttendedParksContext);
 
+  const [attendingParkDropdown, setAttendingParkDropdown] =
+    useState<boolean>(false);
+  const [completingActivityDropdown, setCompletingActivityDropdown] =
+    useState<boolean>(false);
+  const [removeParkDropdown, setRemoveParkDropdown] = useState<boolean>(false);
+
   return (
     <div className="Account">
       <h2>Hello, {user?.displayName}!</h2>
@@ -20,6 +26,56 @@ const Account = () => {
               <AccountParkCard park={park} key={park._id} />
             )
         )}
+      </ul>
+      <h2>Acount Parks FAQ</h2>
+      <ul>
+        <li>
+          <div>
+            <h3>Attending Parks</h3>
+            <button onClick={() => setAttendingParkDropdown((prev) => !prev)}>
+              Dropdown
+            </button>
+          </div>
+          {attendingParkDropdown && (
+            <p>
+              You can add parks to your visited list on your account by clicking
+              the attended button. Your visited parks will be saved onto your
+              account when ever you log back into your account.
+            </p>
+          )}
+        </li>
+        <li>
+          <div>
+            <h3>Completing Park Activities</h3>
+            <button
+              onClick={() => setCompletingActivityDropdown((prev) => !prev)}
+            >
+              Dropdown
+            </button>
+          </div>
+          {completingActivityDropdown && (
+            <p>
+              When completing an activity, your progress will increase until you
+              have reached 100% park completion. You can unset activities that
+              you have not completed.
+            </p>
+          )}
+        </li>
+        <li>
+          <div>
+            <h3>Removing Park</h3>
+            <button onClick={() => setRemoveParkDropdown((prev) => !prev)}>
+              Dropdown
+            </button>
+          </div>
+          {removeParkDropdown && (
+            <p>
+              If you delete a visited park from you saved list, all the progress
+              will be deleted as well. Please be aware when deleting parks you
+              delete.
+            </p>
+          )}
+        </li>
       </ul>
     </div>
   );
