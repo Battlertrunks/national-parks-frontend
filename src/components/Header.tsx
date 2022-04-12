0f5aa7fe68bb7ff480327453e5fccc75eb7e098d
Thomas Hudak <thudak734@gmail.com>
2022-04-12 14:19:00 -0400

    sandwich menu is made

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { signInWithGoogle, signOut } from "../firebaseConfig";
@@ -6,7 +6,9 @@ import "./Header.css";
 
 const Header = () => {
   const { user } = useContext(AuthContext);
-  console.log(user);
+  const [menuDropdown, setMenuDropdown] = useState<boolean>(true);
+
+  const setDropdown = menuDropdown ? "menu-dropdown" : "menu-dropdown-open";
 
   return (
     <header className="Header">
@@ -14,23 +16,12 @@ const Header = () => {
         <h1>National Treasures</h1>
       </Link>
       <div>
-        <ul>
-          <li>
-            <Link to="/parks/search">Search</Link>
-          </li>
-          <li>
-            <Link to="/parks/aboutus">About Us</Link>
-          </li>
-        </ul>
         {user ? (
           <div>
             <Link to="/account">
-
               <p className="username">{user.displayName}</p>
 
               {user.photoURL && <img src={user.photoURL} alt="Profile Image" />}
-              
-
             </Link>
             <button onClick={signOut}>Sign Out</button>
           </div>
@@ -38,9 +29,26 @@ const Header = () => {
           <button onClick={signInWithGoogle}>Sign In</button>
         )}
 
-        <button className="sideBar">
+        <button
+          onClick={() => setMenuDropdown((prev) => !prev)}
+          className="sideBar"
+        >
           <i className="fa-solid fa-bars"></i>
         </button>
+        <ul className={setDropdown}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/parks/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/parks/aboutus">About Us</Link>
+          </li>
+          <li>
+            <Link to="/account">Account</Link>
+          </li>
+        </ul>
       </div>
     </header>
   );
