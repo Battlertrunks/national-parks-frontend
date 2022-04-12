fae485ec2478832ec1e5696575177382e76a7d60
Gavin <Gavinszczesniak@gmail.com>
2022-04-12 19:35:13 -0400

    Users can now like and unlike posts on the social media page.

--- src/components/SocialMediaRoute.tsx
+++ src/components/SocialMediaRoute.tsx
@@ -20,6 +20,14 @@ const SocialMediaRoute = () => {
     likePost(likedPost._id!, likedPost);
   };
 
+  const unlikeAPost = (unlikedPost: PostModel): void => {
+    unlikedPost.likes.amountOfLikes -= 1;
+    unlikedPost.likes.uids = unlikedPost.likes.uids.filter(
+      (keepUser) => keepUser !== user?.uid
+    );
+    likePost(unlikedPost._id!, unlikedPost);
+  };
+
   const deleteYourPost = (postToDelete: string): void => {
     deleteUserPost(postToDelete);
   };
@@ -41,8 +49,10 @@ const SocialMediaRoute = () => {
             <h3>{post?.title}</h3>
             <img src={post.imageURL} alt={post.imageURL} />
             <p>{post?.body}</p>
-            {user && !post.likes.uids.some((users) => users === user.uid) && (
+            {user && !post.likes.uids.some((users) => users === user.uid) ? (
               <button onClick={() => likingAPost(post)}>Like</button>
+            ) : (
+              <button onClick={() => unlikeAPost(post)}>Unlike</button>
             )}
             <p>Likes: {post.likes.amountOfLikes}</p>
           </li>
