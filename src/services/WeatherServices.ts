7e971a7ae85a2ba8e8e3c3adeefa8b13d041dd34
AsianPhoenix <alex.sweezie@gmail.com>
2022-04-14 13:45:41 -0400

    sorry Tom, I was in your card

--- src/services/WeatherServices.ts
+++ src/services/WeatherServices.ts
@@ -4,7 +4,7 @@ import WeatherModel from "../models/WeatherModel";
 // The API key to access our weather API
 const key: string = process.env.REACT_APP_WEATHER_KEY || "";
 
-// retriving the data of the weather at a location using the zip/postal code
+// retrieving the data of the weather at a location using the zip/postal code
 export const getWeather = async (zipCode: string): Promise<WeatherModel> => {
   return (
     await axios.get("http://api.weatherapi.com/v1/current.json", {
