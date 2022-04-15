a98cc066425e1e5526960adb6cbaa7c95668bc04
Thomas Hudak <thudak734@gmail.com>
2022-04-15 13:54:26 -0400

    cleaned up code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,82 +14,6 @@ const Header = () => {
   // We store our links here to go to different pages.
   // Users can return to homepage when they click our logo too.
   return (
-    //     <header className="Header">
-    //       <Link to="/">
-    //         <h1>National Treasures</h1>
-    //       </Link>
-
-    //       <ul className="navlinks">
-    //         <li>
-    //           <Link to="/">Home</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/parks/search">Search</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/parks/aboutus">About Us</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/account">Account</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/posts">Blog</Link>
-    //         </li>
-    //       </ul>
-
-    //       <div>
-    //         {user ? (
-    //           <div className="signInContainer">
-    //             <Link to="/account">
-    //               <p className="username">
-    //                 {user.displayName}
-    //                 {user.photoURL && (
-    //                   <img
-    //                     src={user.photoURL}
-    //                     alt="Profile Image"
-    //                     className="profileImg"
-    //                   />
-    //                 )}
-    //               </p>
-    //             </Link>
-    //             <button className="signOutBtn" onClick={signOut}>
-    //               Sign Out
-    //             </button>
-    //           </div>
-    //         ) : (
-    //           <button className="signInBtn" onClick={signInWithGoogle}>
-    //             Sign In
-    //           </button>
-    //         )}
-    //         <div className="dropdown">
-    //           <button>
-    //             <i className="fa-solid fa-bars"></i>
-    //           </button>
-    //           <div className="dropdown-content">
-    //             <ul>
-    //               <li>
-    //                 <Link to="/">Home</Link>
-    //               </li>
-    //               <li>
-    //                 <Link to="/parks/search">Search</Link>
-    //               </li>
-    //               <li>
-    //                 <Link to="/parks/aboutus">About Us</Link>
-    //               </li>
-    //               <li>
-    //                 <Link to="/account">Account</Link>
-    //               </li>
-    //               <li>
-    //                 <Link to="/posts">Blog</Link>
-    //               </li>
-    //             </ul>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </header>
-    //   );
-    // };
-
     <header className="Header">
       <Link to="/">
         <h1>National Treasures</h1>
@@ -116,18 +40,20 @@ const Header = () => {
         <div className="userInfo">
           {user ? (
             <div className="signInContainer">
-              <Link to="/account">
-                <p className="username">
-                  {user.displayName}
-                  {user.photoURL && (
-                    <img
-                      src={user.photoURL}
-                      alt="Profile Image"
-                      className="profileImg"
-                    />
-                  )}
-                </p>
-              </Link>
+              <div className="name-and-pic">
+                <Link to="/account">
+                  <p className="username">
+                    {user.displayName}
+                    {user.photoURL && (
+                      <img
+                        src={user.photoURL}
+                        alt="Profile Image"
+                        className="profileImg"
+                      />
+                    )}
+                  </p>
+                </Link>
+              </div>
               <div className="disappear-btn">
                 <button className="signOutBtn" onClick={signOut}>
                   Sign Out
